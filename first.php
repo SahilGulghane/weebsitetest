f3da5f0ad9b65b4510d24edcfbeaea4591941f16
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2023-05-24 02:56:17 +0530

    Update first.php

--- first.php
+++ first.php
@@ -1,1 +1,34 @@
-hello herllll
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Counter</title>
+</head>
+<body>
+    <?php
+    // Check if the counter file exists
+    if (file_exists('counter.txt')) {
+        // Read the current count from the file
+        $count = (int) file_get_contents('counter.txt');
+    } else {
+        // Counter file doesn't exist, initialize count to 0
+        $count = 0;
+    }
+
+    // Check if the form is submitted
+    if (isset($_POST['submit'])) {
+        // Increment the count
+        $count++;
+        // Save the updated count to the file
+        file_put_contents('counter.txt', $count);
+    }
+    ?>
+    
+    <h1>Counter</h1>
+    
+    <p>Count: <?php echo $count; ?></p>
+    
+    <form method="post">
+        <button type="submit" name="submit">Increment</button>
+    </form>
+</body>
+</html>
