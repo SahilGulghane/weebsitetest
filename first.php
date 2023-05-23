8e633048a1566e8d7ac1abca06b83f178c16b60a
Sahil Gulghane <101868182+SahilGulghane@users.noreply.github.com>
2023-05-24 03:14:21 +0530

    Update first.php

--- first.php
+++ first.php
@@ -1,34 +1,46 @@
+<?php
+session_start();
+
+// Check if the counter is already set in session
+if (!isset($_SESSION['counter'])) {
+    // Counter is not set, initialize it to 0
+    $_SESSION['counter'] = 0;
+}
+
+// Increment the counter
+if (isset($_POST['increment'])) {
+    if ($_SESSION['counter'] < 100) {
+        $_SESSION['counter']++;
+    }
+}
+
+// Decrement the counter
+if (isset($_POST['decrement'])) {
+    if ($_SESSION['counter'] > 0) {
+        $_SESSION['counter']--;
+    }
+}
+
+// Reset the counter
+if (isset($_POST['reset'])) {
+    $_SESSION['counter'] = 0;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Counter</title>
 </head>
 <body>
-    <?php
-    // Check if the counter file exists
-    if (file_exists('counter.txt')) {
-        // Read the current count from the file
-        $count = (int) file_get_contents('counter.txt');
-    } else {
-        // Counter file doesn't exist, initialize count to 0
-        $count = 0;
-    }
-
-    // Check if the form is submitted
-    if (isset($_POST['submit'])) {
-        // Increment the count
-        $count++;
-        // Save the updated count to the file
-        file_put_contents('counter.txt', $count);
-    }
-    ?>
-    
     <h1>Counter</h1>
-    
-    <p>Count: <?php echo $count; ?></p>
-    
+
+    <p>Count: <?php echo $_SESSION['counter']; ?></p>
+
     <form method="post">
-        <button type="submit" name="submit">Increment</button>
+        <button type="submit" name="increment">Increment</button>
+        <button type="submit" name="decrement">Decrement</button>
+        <button type="submit" name="reset">Reset</button>
     </form>
 </body>
 </html>
